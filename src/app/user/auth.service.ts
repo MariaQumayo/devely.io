4e3d5542dd871861e7677a39973bb75741e463e4
Maria Qumayo <maria.qumayo@is.co.za>
2020-07-29 20:16:49 +0200

    Confirm user with generated key done

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -55,7 +55,20 @@ export class AuthService {
     this.authIsLoading.next(true);
     const userData = {
       Username: username,
+      Pool: userPool
     };
+    const cognitoUser = new CognitoUser(userData);
+    cognitoUser.confirmRegistration(code,true, (err, result) =>{
+      if(err){
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        console.log('I FAILI BECAUSE', err);
+        return;
+      }
+      this.authDidFail.next(false);
+      this.authIsLoading.next(false);
+      this.router.navigate(['/']);
+    });
   }
   signIn(username: string, password: string): void {
     this.authIsLoading.next(true);
